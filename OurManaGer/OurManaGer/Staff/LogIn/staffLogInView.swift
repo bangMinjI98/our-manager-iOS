99b602c981d9cd80a4dc05b1d9b693c551057ecd
bangMinji98 <whoru9806@gmail.com>
2023-11-16 23:00:13 +0900

    ADD: 프로젝트 초기세팅 및 파일링

---
+++ OurManaGer/OurManaGer/Staff/LogIn/staffLogInView.swift
@@ -0,0 +1,24 @@
+//
+//  staffLogInView.swift
+//  OurManaGer
+//
+//  Created by 방민지 on 11/16/23.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+
+class staffLogInView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+    }
+    
+    private func setLayout() {
+        view.backgroundColor = UIColor(red: 0.949, green: 0.906, blue: 0.757, alpha: 0.8)
+    }
+
+}
---
+++ OurManaGer/OurManaGer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurManaGer
+//
+//  Created by 방민지 on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
